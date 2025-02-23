6eb6ab23dcae9820051117509a81e0fe1bb86579
ANkit Ojha <ankitojha1409@gmail.com>
2025-02-23 22:24:06 +0530

    unused packanges

--- config/upstash.js
+++ config/upstash.js
@@ -3,8 +3,8 @@ import { Client as WorkflowClient } from "@upstash/workflow";
 import {
   QSTASH_URL,
   QSTASH_TOKEN,
-  QSTASH_NEXT_SIGNING_KEY,
-  QSTASH_CURRENT_SIGNING_KEY,
+  //   QSTASH_NEXT_SIGNING_KEY,
+  //   QSTASH_CURRENT_SIGNING_KEY,
 } from "./env";
 
 export const workflowClient = new WorkflowClient({
